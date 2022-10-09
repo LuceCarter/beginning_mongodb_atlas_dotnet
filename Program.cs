96a10f8927ac45e8355c574975610b7a70059f64
Luce Carter <luce@lucyscarter.co.uk>
2022-10-09 11:04:37 +0100

    Started to implement a DatabaseService, moved MongoDB init code from program.cs to new service and added to builder

--- Program.cs
+++ Program.cs
@@ -1,23 +1,14 @@
 using MongoDB.Driver;
 using MongoDB.Bson;
-
+using beginning_mongodb_atlas_dotnet.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Fetch our connection string from appsettings which is available in the builder's Configuration
 var connectionString = builder.Configuration["GamesDatabaseSettings:ConnectionString"];
 
-MongoClient mongoDBClient = new MongoClient(connectionString);
-
-List<string> databases = mongoDBClient.ListDatabaseNames().ToList();
-
-foreach(string db in databases)
-{
-    Console.WriteLine(db);
-
-}
-
 // Add services to the container.
+builder.Services.AddSingleton(new DatabaseService(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace beginning_mongodb_atlas_dotnet.Services
+{
+    public class DatabaseService
+    {
+        public DatabaseService(string connectionString)
+        {
+            MongoClient mongoDBClient = new MongoClient(connectionString);
+            List<string> databases = mongoDBClient.ListDatabaseNames().ToList();
+
+            foreach (string db in databases)
+            {
+                Console.WriteLine(db);
+
+            }
+        }
+    }
+}
